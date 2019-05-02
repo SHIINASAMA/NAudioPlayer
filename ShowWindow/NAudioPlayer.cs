6c9c1fd0acfbe9bbfaee3de6db267f7398b4b9d5
SHIINASAMA <SHIINASAMA@github.com>
2019-05-02 21:20:11 +0800

    更新了例子

---
+++ ShowWindow/NAudioPlayer.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio;
+using NAudio.Wave;
+
+namespace NAudioPlayer
+{
+    public class NAudioPlayer
+    {
+        private IWavePlayer wavePlayer;
+        private AudioFileReader audioFileReader;
+
+        private bool isPlaying = false;
+        /// <summary>
+        /// 当前的播放状态
+        /// </summary>
+        public bool IsPlaying
+        {
+            get
+            {
+                return isPlaying;
+            }
+        }
+
+        private string songName = "";
+        /// <summary>
+        /// 播放文件路径及名称
+        /// </summary>
+        public string SongName
+        {
+            get
+            {
+                return songName;
+            }
+        }
+
+        private float volume = 0.5f;
+        /// <summary>
+        /// 播放器声音大小，默认为50%
+        /// </summary>
+        public float Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value >= 0 && value <= 1f)
+                {
+                    volume = value;
+                    if (audioFileReader != null)
+                    {
+                        audioFileReader.Volume = value;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public NAudioPlayer()
+        {
+
+        }
+
+        /// <summary>
+        /// 初始化播放器,初始化参数为播放文件路径
+        /// </summary>
+        public void Init(string SongName)
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+            }
+
+            if (string.IsNullOrEmpty(SongName))
+                return;
+
+            this.songName = SongName;
+
+            if(wavePlayer != null)
+            {
+                wavePlayer.Dispose();
+                GC.Collect();
+            }
+
+            wavePlayer = new WaveOut();
+            audioFileReader = new AudioFileReader(songName);
+            audioFileReader.Volume = Volume;
+            wavePlayer.Init(audioFileReader);
+            wavePlayer.PlaybackStopped += WavePlayer_PlaybackStopped;
+            wavePlayer.Pause();
+        }
+
+        private void WavePlayer_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (audioFileReader != null)
+            {
+                audioFileReader.Dispose();
+                audioFileReader = null;
+            }
+            if (wavePlayer != null)
+            {
+                wavePlayer.Dispose();
+                wavePlayer = null;
+            }
+
+            isPlaying = false;
+        }
+
+        /// <summary>
+        /// 获取当前播放位置
+        /// </summary>
+        public TimeSpan CurrentTime
+        {
+            get
+            {
+                if (audioFileReader == null)
+                {
+                    return TimeSpan.Zero;
+                }
+                else
+                {
+                    return audioFileReader.CurrentTime;
+                }
+
+            }
+            set
+            {
+                audioFileReader.CurrentTime = value;
+            }
+        }
+
+        /// <summary>
+        /// 获取总长度
+        /// </summary>
+        public TimeSpan TotalTime
+        {
+            get
+            {
+                if (audioFileReader == null)
+                {
+                    return TimeSpan.Zero;
+                }
+                else
+                {
+                    return audioFileReader.TotalTime;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 开始播放或者继续播放
+        /// </summary>
+        public void Play()
+        {
+            if (isPlaying)
+            {
+                return;
+            }
+
+            if (wavePlayer == null)
+            {
+                return;
+            }
+
+            wavePlayer.Play();
+            isPlaying = true;
+        }
+
+        //暂停
+        public void Pause()
+        {               
+            if (wavePlayer != null)
+            {
+                wavePlayer.Pause();
+                isPlaying = false;
+            }
+        }
+    }
+}
---
+++ ShowWindow/ShowWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShowWindow
+{
+    public partial class ShowWindow : Form
+    {
+        NAudioPlayer.NAudioPlayer player;
+
+        public ShowWindow()
+        {
+            InitializeComponent();
+
+            player = new NAudioPlayer.NAudioPlayer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            player.Init("アイ.mp3");
+            player.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Init("雪が弾けたその場所で.MP3");
+            player.Play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (player.IsPlaying)
+                player.Pause();
+            else
+                player.Play();
+        }
+    }
+}
